28674e0d13c7e21161cf08bb621e5d8f2c84451d
NeroXenovBot <149504056+NeroXenovBot@users.noreply.github.com>
2023-12-05 15:25:28 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -42,5 +42,5 @@ class Var:
         "THUMBNAIL", default="https://telegra.ph/file/a68a60ec470d08fee3945.jpg"
     )
     FFMPEG = config("FFMPEG", default="ffmpeg")
-    SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
+    SEND_SCHEDULE = config("SEND_SCHEDULE", default=True, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
