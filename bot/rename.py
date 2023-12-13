9be9418ab3196b20d61ff65922194bf840835d96
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 04:02:45 +0100

    Update rename.py

--- bot/rename.py
+++ bot/rename.py
@@ -23,6 +23,8 @@ anilist = Anilist()
 CAPTION = """
 <strong>{}</strong>
 
+➜ <b>Powered By :</b> <a href=https://t.me/Roofiverse>𝖱𝗈𝗈𝖿𝗂𝗏𝖾𝗋𝗌𝖾</a> & <a href=https://t.me/FuZionX>𝖥𝗎𝖹𝗂𝗈𝗇𝖷</a>
+
 {}
 """
 
